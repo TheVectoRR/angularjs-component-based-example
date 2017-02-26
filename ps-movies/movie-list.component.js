d50a33073f38c29d1acd4920d0af07c4f8f34421
Jonathan <jonathanoudshoorn@gmail.com>
2017-02-26 20:01:41 +0100

    moving the controller functionality into a separate function

--- ps-movies/movie-list.component.js
+++ ps-movies/movie-list.component.js
@@ -6,16 +6,17 @@
     module.component("movieList", {
         templateUrl: "ps-movies/movie-list.component.html",
         controllerAs: "model",
-        controller: function(){
-
-            var model = this;
-
-            model.message = "hello from a component controller!";
-
-            model.changeMessage = function(){
-                model.message = "New Message";
-            }
-        }
+        controller: controller
     });
+
+    function controller(){
+        var model = this;
+
+        model.message = "hello from a component controller!";
+
+        model.changeMessage = function(){
+            model.message = "New Message";
+        }
+    }
 
 }());
