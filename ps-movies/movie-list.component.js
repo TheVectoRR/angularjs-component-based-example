3ef26c0611d1042ca739ec2f86841fb885286180
Jonathan <jonathanoudshoorn@gmail.com>
2017-02-26 19:51:30 +0100

    demonstrating use of controller in a component and also demonstrating 2-way binding

--- ps-movies/movie-list.component.js
+++ ps-movies/movie-list.component.js
@@ -7,7 +7,14 @@
         templateUrl: "ps-movies/movie-list.component.html",
         controllerAs: "model",
         controller: function(){
-            this.message = "hello from a component controller!";
+
+            var model = this;
+
+            model.message = "hello from a component controller!";
+
+            model.changeMessage = function(){
+                model.message = "New Message";
+            }
         }
     });
 
