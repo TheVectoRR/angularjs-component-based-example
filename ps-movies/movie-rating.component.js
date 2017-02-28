3f33518ab6101374ae2ac9be8f035017a037925a
Jonathan <jonathanoudshoorn@gmail.com>
2017-02-28 19:20:00 +0100

    Building decoupled components (rating-component)

--- ps-movies/movie-rating.component.js
+++ ps-movies/movie-rating.component.js
@@ -6,20 +6,32 @@
     module.component("movieRating", {
         templateUrl: "ps-movies/movie-rating.component.html",
         bindings:{
-            value: "<"
+            value: "<",
+            max: "<"
         },
-        transclude: true, // a flag allowing saying this component brings html-content from the outside world.
         controllerAs: "model",
         controller: function(){
             var model = this;
 
             model.$onInit = function(){
-                model.entries = new Array(model.value); // array because of ng-repeat
+                model.entries = buildEntries(model.value, model.max);
             };
 
             model.$onChanges = function(){
-                model.entries = new Array(model.value);
+                model.entries = buildEntries(model.value, model.max);
             };
         }
     });
+
+    function buildEntries(value, max) {
+        var entries = [];
+
+        for (var i = 1; i <= max; i++) {
+            var icon = i <= value ? "glyphicon-star" : "glyphicon-star-empty";
+            entries.push(icon);
+        }
+
+        return entries;
+    }
+
 }());
